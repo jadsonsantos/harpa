f74ee3850131523256efbc93e07bd10495727901
Jadson Santos <jadsondossantos5@gmail.com>
2024-05-31 21:59:00 -0300

    feat: create container

---
+++ src/components/Container/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles'
+
+type Props = {
+  children: React.ReactNode
+}
+
+const Container = ({ children }: Props) => <S.Wrapper>{children}</S.Wrapper>
+
+export default Container
---
+++ src/components/Container/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  width: 100%;
+  max-width: 80rem;
+  margin: 0 auto;
+  padding: 0 24px;
+`
