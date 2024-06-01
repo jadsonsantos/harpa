31b9fafd5f25377cbcbe6247dfd71d5ca9509c1a
Jadson Santos <jadsondossantos5@gmail.com>
2024-05-31 22:05:59 -0300

    feat: create header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import Container from '../Container'
+import * as S from './styles'
+
+const Header = () => (
+  <S.Wrapper>
+    <Container>
+      <h1>Header</h1>
+    </Container>
+  </S.Wrapper>
+)
+
+export default Header
