84ee1e6bae41304c5fda0f97257cb90bdc02b226
Jadson Santos <jadsondossantos5@gmail.com>
2024-06-04 20:58:58 -0300

    button component

---
+++ src/components/Button/stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react'
+import Button from '.'
+
+export default {
+  title: 'Button',
+  component: Button
+} as Meta
+
+export const Default: StoryObj = {}
