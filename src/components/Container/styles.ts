7034e5244606cb91478db6c524338c8db2bf0a51
Jadson Santos <jadsondossantos5@gmail.com>
2024-06-04 21:00:01 -0300

    container style

--- src/components/Container/styles.ts
+++ src/components/Container/styles.ts
@@ -1,8 +1,17 @@
+import { media } from '@/styles/mediaQueries'
 import styled from 'styled-components'
 
 export const Wrapper = styled.div`
   width: 100%;
-  max-width: 80rem;
+  max-width: 1600px;
   margin: 0 auto;
   padding: 0 24px;
+
+  ${media.tabletUp} {
+    padding: 0 64px;
+  }
+
+  ${media.desktopUp} {
+    /* padding: 0 128px; */
+  }
 `
