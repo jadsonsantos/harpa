877e0082930b03b664fbb9ade29fc7d421e0fe18
Jadson Santos <jadsondossantos5@gmail.com>
2024-06-08 17:24:05 -0300

    header adjustments

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,24 @@
+import useIsMobile from '@/hooks/isMobile'
 import Container from '../Container'
 import Logo from '../Logo'
 import Menu from '../Menu'
 import * as S from './styles'
 
-const Header = () => (
-  <S.Wrapper>
-    <Container>
-      <Logo />
-      <Menu />
-    </Container>
-  </S.Wrapper>
-)
+const Header = () => {
+  const isMobile = useIsMobile()
+
+  return (
+    <S.Wrapper>
+      <Container>
+        {isMobile ? (
+          <Logo width={10} height={18} imgSrc="harpa-logo-mobile" />
+        ) : (
+          <Logo />
+        )}
+        <Menu />
+      </Container>
+    </S.Wrapper>
+  )
+}
 
 export default Header
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,7 +1,13 @@
+import { colors } from '@/styles/tokens'
 import styled from 'styled-components'
 
 export const Wrapper = styled.header`
-  padding: 30px 0;
+  position: sticky;
+  top: 0;
+  z-index: 1;
+  height: 82px;
+  display: flex;
+  background-color: ${colors.light};
 
   & > div {
     display: flex;
