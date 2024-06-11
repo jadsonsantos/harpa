f61b242410f9890fad7d47020cb182ae2c32abb0
Jadson Santos <jadsondossantos5@gmail.com>
2024-06-11 20:53:02 -0300

    feat: create template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,12 @@
 'use client'
-import Contact from '@/components/Contact'
-import Footer from '@/components/Footer'
-import Header from '@/components/Header'
 import HeroHome from '@/components/HeroHome'
-import News from '@/components/News'
 import Partners from '@/components/Partners'
 
 export default function Home() {
   return (
     <>
-      <Header />
       <HeroHome title="<span>Islândia</span><br/> Terra do gelo e do fogo" />
-      <main>
-        <Partners />
-      </main>
-      <News />
-      <Contact />
-      <Footer />
+      <Partners />
     </>
   )
 }
--- src/app/quem-somos/page.tsx
+++ src/app/quem-somos/page.tsx
@@ -1,24 +1,14 @@
 'use client'
-import Contact from '@/components/Contact'
-import Footer from '@/components/Footer'
 import Founder from '@/components/Founder'
 import Harpa from '@/components/Harpa'
-import Header from '@/components/Header'
 import HeroAbout from '@/components/HeroAbout'
-import News from '@/components/News'
 
 export default function About() {
   return (
     <>
-      <Header />
-      <main>
-        <HeroAbout title="A ideia nasce na vontade do <span>fazer bem feito</span>" />
-        <Harpa />
-        <Founder />
-      </main>
-      <News />
-      <Contact />
-      <Footer />
+      <HeroAbout title="A ideia nasce na vontade do <span>fazer bem feito</span>" />
+      <Harpa />
+      <Founder />
     </>
   )
 }
