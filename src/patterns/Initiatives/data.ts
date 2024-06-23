38fc14ec3a9b717524c201c6a460174b414feffb
Jadson Santos <jadsondossantos5@gmail.com>
2024-06-23 13:16:55 -0300

    fix build error

--- src/patterns/Initiatives/data.ts
+++ src/patterns/Initiatives/data.ts
@@ -1,13 +1,15 @@
 const data = [
   {
     title: 'Mentorias customizadas',
-    description:
+    description: [
       'Em nossas mentorias, você aprenderá Inglês de forma disruptiva e com conteúdos 100% customizados. Isso quer dizer que desenharemos, juntos, o plano de aulas, usando como base o seu assessment pessoal e as suas atuais prioridades.'
+    ]
   },
   {
     title: 'Desenvolvimento de conteúdos',
-    description:
+    description: [
       'Desenvolvemos e aplicamos conteúdos de educação executiva customizados em inglês. Nós nos baseamos sempre no diagnóstico feito após uma boa conversa com a sua empresa. Agende uma visita e levamos todas a ideias!'
+    ]
   }
 ]
 
