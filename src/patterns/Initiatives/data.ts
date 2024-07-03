2a215f71f141a68bcbf11f9e2a29e8120442c661
Jadson Santos <jadsondossantos5@gmail.com>
2024-07-02 23:04:16 -0300

    initiatives component

--- src/patterns/Initiatives/data.ts
+++ src/patterns/Initiatives/data.ts
@@ -3,30 +3,59 @@ import { WHATSSAPP_URL } from '@/constants'
 const data = [
   {
     title: 'Soluções educacionais',
-    description: [
-      '<strong>Mentorias customizadas</strong>',
-      `Em nossas mentorias, você aprenderá Inglês de forma disruptiva e com conteúdos 100% customizados. Isso quer dizer que desenharemos, juntos, o plano de aulas, usando como base o seu assessment pessoal e as suas atuais prioridades.`,
-      '<strong>Traduções</strong>',
-      `Traduzimos todos os materiais não juramentados: contratos, teses, websites, publicidade, vídeos, roteiros, entre outros.`,
-      '<strong>English performance</strong>',
-      `No Performance, o aluno aprenderá habilidades como: construção e uso da retórica, apresentações executivas, oratória e entonações e uso assertivo de vocabulário.`,
-      '<strong>Desenvolvimento de conteúdos</strong>',
-      `Desenvolvemos e aplicamos conteúdos de educação executiva customizados em inglês. Nós nos baseamos sempre no diagnóstico feito após uma boa conversa com a sua empresa. Agende uma visita e levamos todas a ideias!`,
-      '<strong>Revisões</strong>',
-      'Você poderá contar com a gente sempre que precisar fazer revisões e transcrições tanto em português como em inglês.',
-      '<strong>Mediações</strong>',
-      `Trabalhamos em múltiplas formas no quesito mediação, em workshops, apresentações, painéis de talks, aplicações de treinamentos bilíngues, entre outras.`
+    content: [
+      {
+        title: 'Mentorias customizadas',
+        description: [
+          `Em nossas mentorias, você aprenderá Inglês de forma disruptiva e com conteúdos 100% customizados. Isso quer dizer que desenharemos, juntos, o plano de aulas, usando como base o seu assessment pessoal e as suas atuais prioridades.`
+        ]
+      },
+      {
+        title: 'Desenvolvimento de conteúdos',
+        description: [
+          `Desenvolvemos e aplicamos conteúdos de educação executiva customizados em inglês. Nós nos baseamos sempre no diagnóstico feito após uma boa conversa com a sua empresa. Agende uma visita e levamos todas a ideias!`
+        ]
+      },
+      {
+        title: 'Traduções',
+        description: [
+          `Traduzimos todos os materiais não juramentados: contratos, teses, websites, publicidade, vídeos, roteiros, entre outros.`
+        ]
+      },
+      {
+        title: 'Revisões',
+        description: [
+          `Você poderá contar com a gente sempre que precisar fazer revisões e transcrições tanto em português como em inglês.`
+        ]
+      },
+      {
+        title: 'English performance',
+        description: [
+          `No Performance, o aluno aprenderá habilidades como: construção e uso da retórica, apresentações executivas, oratória e entonações e uso assertivo de vocabulário.`
+        ]
+      },
+      {
+        title: 'Mediações',
+        description: [
+          `Trabalhamos em múltiplas formas no quesito mediação, em workshops, apresentações, painéis de talks, aplicações de treinamentos bilíngues, entre outras.`
+        ]
+      }
     ],
     cta: {
       text: 'ENTRE EM CONTATO E SAIBA MAIS >',
       href: WHATSSAPP_URL
-    }
+    },
+    hasAccordion: true
   },
   {
     title: 'HÁRPÁ para empresas',
-    description: [
-      '<strong>Serviços customizados para empresas</strong>',
-      'Condições especiais para empresas ou grandes turmas. Entre em contato para saber mais.'
+    content: [
+      {
+        title: 'Serviços customizados para empresas',
+        description: [
+          'Condições especiais para empresas ou grandes turmas. Entre em contato para saber mais.'
+        ]
+      }
     ],
     cta: {
       text: 'ENTRE EM CONTATO E SAIBA MAIS >',
@@ -35,9 +64,13 @@ const data = [
   },
   {
     title: 'Collab',
-    description: [
-      '<strong>Parcerias de sucesso</strong>',
-      'Para parcerias entre sua empresa e a HÁRPA, entre em contato conosco e saiba como podemos colaborar.'
+    content: [
+      {
+        title: '<strong>Parcerias de sucesso</strong>',
+        description: [
+          'Para parcerias entre sua empresa e a HÁRPA, entre em contato conosco e saiba como podemos colaborar.'
+        ]
+      }
     ],
     cta: {
       text: 'ENTRE EM CONTATO >',
--- src/patterns/Initiatives/index.tsx
+++ src/patterns/Initiatives/index.tsx
@@ -1,5 +1,6 @@
 import AnimatedIllustration from '@/components/AnimatedIllustration'
 import TextGrid from '@/components/TextGrid'
+import { Fragment } from 'react'
 import data from './data'
 import * as S from './styles'
 
@@ -8,9 +9,13 @@ const Initiatives = () => (
     <S.InitiativesContainer>
       <AnimatedIllustration />
       <S.InitiativesText>
-        {data.map((data) => (
-          <TextGrid key={data.title} {...data} />
-        ))}
+        {data.map((item) => {
+          return (
+            <Fragment key={item.title}>
+              <TextGrid {...item} />
+            </Fragment>
+          )
+        })}
       </S.InitiativesText>
     </S.InitiativesContainer>
   </S.Wrapper>
