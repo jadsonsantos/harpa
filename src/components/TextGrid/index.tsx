f26d7d2970ee3806f6781711e7bb468d73bf87be
Jadson Santos <jadsondossantos5@gmail.com>
2024-07-02 23:01:50 -0300

    textgrid component

--- src/components/TextGrid/index.tsx
+++ src/components/TextGrid/index.tsx
@@ -1,25 +1,60 @@
+'use client'
+import useIsMobile from '@/hooks/isMobile'
+import Details from '../Details'
 import Heading from '../Heading'
 import Text from '../Text'
 import * as S from './styles'
 
-interface TextGridProps {
+interface Content {
   title: string
   description: string[]
-  cta?: { href: string; text: string }
 }
 
-const TextGrid = ({ title = '', description, cta }: TextGridProps) => (
-  <S.TextGrid>
-    <Heading>{title}</Heading>
-    {description.map((desc) => (
-      <Text key={desc}>{desc}</Text>
-    ))}
-    {cta && (
-      <Text>
-        <S.Link href={cta.href}>{cta.text}</S.Link>
-      </Text>
-    )}
-  </S.TextGrid>
-)
+interface TextGridProps {
+  title: string
+  description?: string[]
+  content?: Content[]
+  cta?: { href: string; text: string }
+  hasAccordion?: boolean
+}
+
+const TextGrid = ({
+  title = '',
+  description,
+  content,
+  cta,
+  hasAccordion
+}: TextGridProps) => {
+  const isMobile = useIsMobile()
+
+  return (
+    <S.TextGrid>
+      <Heading>{title}</Heading>
+      <S.Content>
+        {content &&
+          content.map(({ title, description }) => {
+            if (isMobile && hasAccordion) {
+              return (
+                <Details key={title} title={title} description={description} />
+              )
+            }
+            return (
+              <div key={title}>
+                <Text>{title}</Text>
+                <Text>{description}</Text>
+              </div>
+            )
+          })}
+        {description &&
+          description.map((desc) => <Text key={desc}>{desc}</Text>)}
+      </S.Content>
+      {cta && (
+        <Text>
+          <S.Link href={cta.href}>{cta.text}</S.Link>
+        </Text>
+      )}
+    </S.TextGrid>
+  )
+}
 
 export default TextGrid
--- src/components/TextGrid/styles.ts
+++ src/components/TextGrid/styles.ts
@@ -9,21 +9,31 @@ export const TextGrid = styled.article`
     margin-bottom: ${spacings.xlarge};
   }
 
-  ${media.tabletUp} {
+  ${media.desktopUp} {
     &:not(:last-of-type) {
       margin-bottom: ${spacings.xxlarge};
     }
   }
 
   p {
-    max-width: 414px;
-
     &:not(:last-of-type) {
       margin-bottom: 20px;
+      font-weight: 700;
     }
   }
 `
 
 export const Link = styled(CustomLink)`
   text-decoration-line: underline;
+`
+
+export const Content = styled.div`
+  margin-bottom: 64px;
+
+  ${media.tabletUp} {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 24px;
+    row-gap: 64px;
+  }
 `
