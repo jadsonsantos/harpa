07f67599360d6d4a5ab93a329e95b2467782fa20
Jadson Santos <jadsondossantos5@gmail.com>
2026-06-21 11:05:14 -0300

    feat: implement locale layout with styled components and metadata generation

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -3,6 +3,8 @@ import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import News from '@/components/News'
 import { routing } from '@/i18n/routing'
+import StyledComponentsRegistry from '@/lib/registry'
+import { inter, playfairDisplay } from '@/styles/fonts'
 import { NextIntlClientProvider } from 'next-intl'
 import {
   getMessages,
@@ -10,12 +12,17 @@ import {
   setRequestLocale
 } from 'next-intl/server'
 import { notFound } from 'next/navigation'
+import { Providers } from '../providers'
 
 export async function generateMetadata({
   params: { locale }
 }: {
   params: { locale: string }
 }) {
+  if (!(routing.locales as readonly string[]).includes(locale)) {
+    notFound()
+  }
+
   const t = await getTranslations({ locale, namespace: 'Metadata' })
 
   return {
@@ -45,13 +52,23 @@ export default async function LocaleLayout({
 
   const messages = await getMessages()
 
+  const fonts = `${inter.variable} ${playfairDisplay.variable}`
+
   return (
-    <NextIntlClientProvider locale={locale} messages={messages}>
-      <Header />
-      {children}
-      <News />
-      <Contact />
-      <Footer />
-    </NextIntlClientProvider>
+    <html lang={locale} className={fonts}>
+      <body>
+        <StyledComponentsRegistry>
+          <Providers>
+            <NextIntlClientProvider locale={locale} messages={messages}>
+              <Header />
+              {children}
+              <News />
+              <Contact />
+              <Footer />
+            </NextIntlClientProvider>
+          </Providers>
+        </StyledComponentsRegistry>
+      </body>
+    </html>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,7 @@
-import StyledComponentsRegistry from '@/lib/registry'
-import { inter, playfairDisplay } from '@/styles/fonts'
-import { Providers } from './providers'
-
-const fonts = `${inter.variable} ${playfairDisplay.variable}`
-
 export default function RootLayout({
   children
 }: {
   children: React.ReactNode
 }) {
-  return (
-    <html className={fonts}>
-      <body>
-        <StyledComponentsRegistry>
-          <Providers>{children}</Providers>
-        </StyledComponentsRegistry>
-      </body>
-    </html>
-  )
+  return children
 }
