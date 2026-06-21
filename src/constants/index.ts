c8e4a3aa92e138c2fd0a5f3ecb0a39883ef6d0ed
Jadson Santos <jadsondossantos5@gmail.com>
2026-06-21 12:14:16 -0300

    feat: add Instagram support and improve Contact component layout

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
-const WHATSSAPP_NUMBER = 5511995842594
-const WHATSSAPP_TEXT =
+export const WHATSSAPP_NUMBER = 5511995842594
+export const WHATSSAPP_TEXT =
   'Olá! Vim do site e gostaria de ter mais informações sobre as expedições para Islândia.'
-const WHATSSAPP_URL = `https://api.whatsapp.com/send?phone=${WHATSSAPP_NUMBER}&text=${WHATSSAPP_TEXT}`
-
-export { WHATSSAPP_NUMBER, WHATSSAPP_TEXT, WHATSSAPP_URL }
+export const WHATSSAPP_URL = `https://api.whatsapp.com/send?phone=${WHATSSAPP_NUMBER}&text=${WHATSSAPP_TEXT}`
+export const INSTAGRAM_URL = 'https://www.instagram.com/harpa.me'
+export const INSTAGRAM_USERNAME = '@harpa.me'
