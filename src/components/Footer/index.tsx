b2bf0bf2f541474d238949cc2d69dccdb2d20451
Jadson Santos <jadsondossantos5@gmail.com>
2026-06-21 11:17:15 -0300

    feat: add internationalization support for Footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,17 +1,19 @@
 'use client'
 import Container from '@/patterns/Container'
+import { useTranslations } from 'next-intl'
 import Link from 'next/link'
 import * as S from './styles'
 
 const Footer = () => {
+  const t = useTranslations('Footer')
   const currentYear = new Date().getFullYear()
 
   return (
     <S.Wrapper>
       <Container>
         <p>
-          Copyright © {currentYear} HÁRPÁ • Todos os direitos reservados •{' '}
-          <Link href="/politica-de-privacidade">Política de Privacidade</Link>
+          {t('copyright', { year: currentYear })} •{' '}
+          <Link href="/politica-de-privacidade">{t('privacyPolicy')}</Link>
         </p>
       </Container>
     </S.Wrapper>
